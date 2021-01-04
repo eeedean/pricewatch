1a2f3795e5a6434a3f12fc444ebe6f73a1f084e5
Dean Eckert <dean.eckert@red-oak-consulting.com>
2021-01-04 20:34:28 +0100

    made "gp"-Links work

--- src/main/java/me/redoak/edean/pricewatch/shops/amazon/AmazonUrlTransformer.java
+++ src/main/java/me/redoak/edean/pricewatch/shops/amazon/AmazonUrlTransformer.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,17 +22,26 @@ public class AmazonUrlTransformer implements UrlTransformer {
 
     @Override
     public URL apply(String url) {
-        if(url == null || !this.appliesFor(url))
+        if (url == null || !this.appliesFor(url))
             throw new TransformationException("Given URL '" + url + "' does not conform to AmazonUrlTransformer.");
 
-        Matcher matcher = Pattern.compile("(dp/[Aa0-Zz9]+)").matcher(url);
-        matcher.find();
-        String match = matcher.group();
+        Optional<String> opt = findId(url);
+        String id = opt.orElseThrow(() -> new RuntimeException("could not find product id in url " + url));
         try {
-            return new URL("https://amazon.de/" + match);
+            return new URL("https://amazon.de/dp/" + id);
         } catch (MalformedURLException e) {
-            throw new TransformationException("There was an issue creating the URL. Found id: " + match, e);
+            throw new TransformationException("There was an issue creating the URL. Found id: " + id, e);
         }
+    }
+
+    private Optional<String> findId(String url) {
+        String captureGroupName = "id";
+        List<Matcher> matchers = Arrays.asList(
+                Pattern.compile("dp/(?<" + captureGroupName + ">[Aa0-Zz9]+)").matcher(url),
+                Pattern.compile("gp/product/(?<" + captureGroupName + ">[Aa0-Zz9]+)").matcher(url)
+
+        );
+        return matchers.stream().filter(Matcher::find).findFirst().map(m -> m.group(captureGroupName));
     }
 
     @Override
